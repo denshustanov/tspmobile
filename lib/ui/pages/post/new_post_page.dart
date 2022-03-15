481b73cf4db6eae781ba84206959064a62ee6390
den.shustanov <den.shustanov@gmail.com>
2022-03-15 17:42:09 +0400

    post image picking

--- lib/ui/pages/post/new_post_page.dart
+++ lib/ui/pages/post/new_post_page.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tspmobile/http_client.dart';
 import 'package:tspmobile/model/post.dart';
 import 'package:tspmobile/model/user.dart';
+import 'package:tspmobile/ui/widgets/pick_image.dart';
+import 'dart:io';
 
 class NewPostPage extends StatefulWidget {
   const NewPostPage({Key? key}) : super(key: key);
@@ -14,21 +19,20 @@ class _NewPostPageState extends State<NewPostPage> {
   TextEditingController _postTextController = new TextEditingController();
   final HttpClient _httpClient = HttpClient();
 
+  List<File> images = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shape: const Border(
-          bottom: BorderSide(
-            color: Colors.grey,
-            width: 1
-          )
-        ),
+        shape: const Border(bottom: BorderSide(color: Colors.grey, width: 1)),
         title: const Text('New post'),
         backgroundColor: Colors.transparent,
         elevation: 0,
         actions: [
-          IconButton(onPressed: ()=>savePost(), icon: const Icon(Icons.done, color: Colors.grey))
+          IconButton(
+              onPressed: () => savePost(),
+              icon: const Icon(Icons.done, color: Colors.grey))
         ],
       ),
       body: Padding(
@@ -48,31 +52,87 @@ class _NewPostPageState extends State<NewPostPage> {
                   cursorWidth: 1,
                   cursorHeight: 15,
                   decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Post text'
-                  ),
+                      border: InputBorder.none, hintText: 'Post text'),
                 ),
               ),
             ),
-            IconButton(onPressed: (){}, icon: const Icon(Icons.image_outlined, color: Colors.grey, size: 30,))
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: images.length,
+                itemBuilder: (context, index){
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                          ),
+                          width: 100,
+                          height: 100,
+                          // alignment: Alignment.centerLeft,
+                          child: Image.file(images.elementAt(index), fit: BoxFit.cover,)
+                        ),
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: IconButton(
+                              onPressed: (){
+                                setState(() {
+                                  images.removeAt(index);
+                                });
+                              },
+                              icon: const Icon(Icons.clear, color: Colors.grey,)
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(
+                    onPressed: () {
+                      if(images.length<10) {
+                        addImages();
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.image_outlined,
+                      color: Colors.grey,
+                      size: 30,
+                    )),
+                Text(images.length.toString() + '/10', style: const TextStyle(color: Colors.grey),)
+              ],
+            )
           ],
         ),
       ),
     );
   }
 
-  Future savePost() async{
-    Post post = Post(
-      _postTextController.text,
-      User(),
-      [],
-      0,
-      0,
-      DateTime.now()
-    );
+  Future savePost() async {
+    Post post =
+        Post(_postTextController.text, User(), [], 0, 0, DateTime.now());
 
     await _httpClient.createPost(post);
 
     Navigator.of(context).pop();
+  }
+
+  Future<void> addImages() async {
+    List<XFile>? imageFiles = await pickMultipleImages();
+    if (imageFiles != null) {
+      for (XFile file in imageFiles) {
+        if(images.length>=10){
+          break;
+        }
+        images.add(File(file.path));
+      }
+      setState(() {});
+    }
   }
 }
